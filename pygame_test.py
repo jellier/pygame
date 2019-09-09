35d1b597d8989f9e45cc1388d2b6a8337af6be20
JiaNa.Xu <jellier@163.com>
2019-09-09 11:11:04 +0800

    Update pygame_test.py

--- pygame_test.py
+++ pygame_test.py
@@ -1,11 +1,13 @@
-# test git
+
 import pygame
+# 每次调用pygame都要使用pygame.init()
 pygame.init()
 screen = pygame.display.set_mode([800,600])
 
 keep_going = True
 radius = 50
 while keep_going:
+    # 此处for循环是处理用户能够在程序中出发的所有交互事件的地方
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keep_going = False
